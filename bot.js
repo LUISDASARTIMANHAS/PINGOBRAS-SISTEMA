6cb5dfc160d5aac06363b66bf65ad68bd64659c6
Glitch (glitch-hello-website) <none>
2022-10-03 11:07:27 +0000

    🏰🚅 Checkpoint ./index.html:26026384/3627 ./style.css:26026384/4158 ./script.js:26026384/920 ./bot.js:26026384/1308 ./config.json:26026384/231 ./package.json:26026384/291

---
+++ bot.js
@@ -0,0 +1,18 @@
+const { Client, version } = require('discord.js');
+const {token} = require('./config.json')
+const bot = new Client();
+
+bot.on("ready", async() => {
+    console.log(`[ Client ] ${bot.user.tag} esta online agora! `);
+    
+    bot.user.setPresence({
+        status: 'idle',
+        activity: {
+            name: ":flag_br: Trabalhando Para Você Crescer! ©️2010-" + new Date().getFullYear(),
+            type: "PLAYING",
+        }
+    })
+})
+
+
+bot.login(token)
