b95ab0ff11271407fe58a875e5b46292e2781774
Glitch (glitch-hello-website) <none>
2022-11-21 17:42:06 +0000

    🍉🌓 Checkpoint ./bot.js:26026384/312

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const {token} = require('./config.json')
 const bot = new Client();
 const date = new Date().getFullYear()
 
+
 bot.on("ready", async() => {
     console.log(`[ Client ] ${bot.user.tag} esta online agora! `);
     
@@ -15,7 +16,7 @@ bot.on("ready", async() => {
     })
   console.log('Bot: Hosting ' + `${bot.users.size}` + ' users, in ' + `${bot.channels.size}` + ' channels of ' + `${bot.guilds.size}` + ' guilds.');
   const channel = await bot.channels.fetch("1032778034811506738");
-  channel.send("MENSAGEM DO SERVIDOR PINGOBRAS: Novo Usuario Conectado A Pingobras:" + navigator.plataform);
+  channel.send("MENSAGEM DO SERVIDOR PINGOBRAS:" + user);
 
     });//Fim do ready
 
