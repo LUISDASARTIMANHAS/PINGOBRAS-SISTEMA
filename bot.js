edccc0025c309f5986b9c54640adf8a6ff0c901f
Glitch (glitch-hello-website) <none>
2022-11-21 18:04:08 +0000

    🚦🏓 Checkpoint ./bot.js:26026384/708

--- bot.js
+++ bot.js
@@ -2,23 +2,27 @@ const { Client, version } = require('discord.js');
 const {token} = require('./config.json')
 const bot = new Client();
 const date = new Date().getFullYear()
-const usersCount = bot.users.size
-const channelsCount = bot.channels
-const guildsCount = bot.guilds
+
 
 
 bot.on("ready", async() => {
-    console.log(`[ Client ] ${bot.user.tag} esta online agora! `);
-    
+const usersCount = bot.users.size
+const channelsCount = bot.channels
+const guildsCount = bot.guilds;
+const botTag = bot.user.tag
+  
+  console.log(`[ Client ] ${botTag} esta online agora! `);
     bot.user.setPresence({
         status: 'idle',activity: {
             name: "Trabalhando Para Você Crescer! ©️2010-" + date ,
             type: "PLAYING",}
     })// bot presença
+  
   console.log('Bot: Hosting ' + usersCount + ' users, in ' + `${channelsCount}` + ' channels of ' + `${guildsCount.size}` + ' guilds.');
   const channel = await bot.channels.fetch("1032778034811506738");
-  channel.send("MENSAGEM DO SERVIDOR PINGOBRAS:");
+  channel.send("MENSAGEM DO SERVIDOR PINGOBRAS:"+ botTag + " Esta online agora!");
 
+  
     });//Fim do ready
 
 bot.login(token)
